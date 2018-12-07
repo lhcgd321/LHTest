1371d6c8a45f82b1bd637926011d7ac79aef0e67
hunter <hunter_long@iot-dreamcatcher.com>
2018-12-07 15:19:50 +0800

    创建了hunter代码

---
+++ hunter/hunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hunter
+//
+//  Created by kd on 2018/12/7.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hunter/hunter/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  hunter
+//
+//  Created by kd on 2018/12/7.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
